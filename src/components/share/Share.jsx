3c0fde1a0855150834472239619289dd39f8b69b
AhmedKamon <ahmedkamon3@gmail.com>
2021-10-23 19:16:53 +0600

    upload image

--- src/components/share/Share.jsx
+++ src/components/share/Share.jsx
@@ -1,24 +1,27 @@
 import { EmojiEmotions, Label, PermMedia, Room } from '@mui/icons-material'
-import { useContext } from 'react'
+import { useContext, useRef, useState } from 'react'
 import { AuthContext } from '../../context/AuthContext'
 import './share.css'
 
 export default function Share() {
     const PF = process.env.REACT_APP_PUBLIC_FOLDER
     const {user} = useContext(AuthContext)
+    const desc = useRef()
+    const [file,setFile] = useState(null)
     return (
         <div className='share' >
             <div className="shareWrapper">
                 <div className="shareTop">
                     <img src={user.profilePicture ? PF+user.profilePicture: PF +'person/noAvater.jpg'} className='shareProfileImage' alt="" />
-                    <input placeholder={"What's in your mind " +user.username+'?'} type="text" className='shareInput' />
+                    <input placeholder={"What's in your mind " +user.username+'?'} type="text" className='shareInput' ref={desc} />
                 </div>
                 <hr className="shareHr" />
-                <div className="shareBottom">
+                <form className="shareBottom">
                     <div className="shareOptions">
                         <div className="shareOption">
                             <PermMedia htmlColor='tomato' className='shareIcon' />
                             <span className='shareOptionText' >Photo or Video</span>
+                            <input type="file"  id="file" accept='.png,.jpg,.jpeg'  onChange={(e) => setFile(e.target.files[0])} />
                         </div>
                         <div className="shareOption">
                             <Label htmlColor='blue' className='shareIcon' />
@@ -35,7 +38,7 @@ export default function Share() {
 
                     </div>
                     <button className='shareButton'>Share</button>
-                </div>
+                </form>
             </div>
         </div>
     )

20c72af73b415424d3fd8bb48ce38cc2e4e2a9e1
AhmedKamon <ahmedkamon3@gmail.com>
2021-10-23 19:02:27 +0600

    like and dislike

--- src/components/post/Post.jsx
+++ src/components/post/Post.jsx
@@ -1,21 +1,31 @@
 import { MoreVert } from '@mui/icons-material'
 import './post.css'
-import {useEffect, useState} from 'react'
+import {useContext, useEffect, useState} from 'react'
 import axios from 'axios'
 import {format} from 'timeago.js'
 import { Link } from 'react-router-dom'
+import { AuthContext } from '../../context/AuthContext'
 
 export default function Post({post}) {
     const [user,setUser]= useState({})
     const [like,setLike] = useState(post.likes.length)
     const [isLiked,setIsLiked] = useState(false)
+    const {user: currentUser} = useContext(AuthContext)
     const PF = process.env.REACT_APP_PUBLIC_FOLDER
+    useEffect(() =>{
+        setIsLiked(post.likes.includes(currentUser._id))
+    },[currentUser._id,post.likes])
+
     const likeHandler = () =>{
+        try {
+            axios.put('/posts/' + post._id + '/like',{userId:currentUser._id})
+        } catch (error) {
+            console.log('failed to fetch from api')
+        }
         setIsLiked(!isLiked)
         setLike(isLiked? like -1: like+ 1)
     }
     useEffect( () =>{
-      
         const findUser = async () =>{
           return await axios.get(`/users?userId=${post?.userId}`).then((res) =>{
             setUser(res.data)
